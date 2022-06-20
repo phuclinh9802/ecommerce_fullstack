d26946c2f92b10d3bd6a20ae7fb27c5ca9a0ddf5
Phuc Nguyen <anicedream2002@gmail.com>
2022-06-20 17:04:58 +0700

    add mongoURI and secret key to .env

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,4 +1,6 @@
+require('dotenv').config()
+
 module.exports = {
-    mongoURI: "mongodb+srv://Phillip:Linhphuc9802@cluster0.qxxpd.azure.mongodb.net/myFirstDatabase?appName=mongosh+1.5.0",
-    secretOrKey: "secret key"
+    mongoURI: process.env.mongoURI,
+    secretOrKey: process.env.secretKey
 }
