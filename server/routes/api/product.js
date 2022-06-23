b5485afce3ac0db1fa7726e2ff2fced4bdab041e
Phillip Nguyen <phucnguyen290198@gmail.com>
2022-06-23 16:22:43 +0700

    fix: incorrect findOne

--- server/routes/api/product.js
+++ server/routes/api/product.js
@@ -1,50 +1,61 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const jwt = require('jsonwebtoken')
-const keys = require('../../config/keys')
-const data = require('../../data/product')
-const Product = require('../../db/Products')
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
+const data = require("../../data/product");
+const Product = require("../../db/Products");
 
-router.get('/', (req, res, next) => {
-  return res.json(data);
-})
+router.get("/", (req, res, next) => {
+  Product.find()
+    .lean()
+    .exec((err, products) => {
+      return res.status(200).json(products);
+    });
+});
 
-router.post('/', (req, res, next) => {
-  token = req.headers['authorization'];
+router.get("/:id", (req, res) => {
+  Product.findOne({ id: req.params.id }).exec((err, product) => {
+    return res.status(200).json(product);
+  });
+});
+
+router.post("/", (req, res, next) => {
+  token = req.headers["authorization"];
 
   // remove Bearer
-  token = token.replace(/^Bearer\s+/, "")
+  token = token.replace(/^Bearer\s+/, "");
 
   if (token) {
     jwt.verify(token, keys.secretOrKey, (err, decoded) => {
       if (err) {
         return res.json({
           success: false,
-          message: 'Token is not valid'
+          message: "Token is not valid",
         });
       } else {
-        Product.find().lean().exec((err, products) => {
+        Product.find()
+          .lean()
+          .exec((err, products) => {
+            const productName = req.body.name;
+            const productImage = req.body.image;
+            const productDescription = req.body.description;
+            const productPrice = req.body.price;
+            const productQuantity = req.body.quantity;
 
-          const productName = req.body.name;
-          const productImage = req.body.image;
-          const productDescription = req.body.description;
-          const productPrice = req.body.price;
-          const productQuantity = req.body.quantity;
+            const newProduct = new Product({
+              name: productName,
+              image: productImage,
+              description: productDescription,
+              price: productPrice,
+              quantity: productQuantity,
+            });
 
-          const newProduct = new Product({
-            name: productName,
-            image: productImage,
-            description: productDescription,
-            price: productPrice,
-            quantity: productQuantity
-          })
-
-          newProduct.save();
-          return res.status(200).json(products)
-        })
+            newProduct.save();
+            return res.status(200).json(products);
+          });
       }
-    })
+    });
   }
-})
+});
 
 module.exports = router;
